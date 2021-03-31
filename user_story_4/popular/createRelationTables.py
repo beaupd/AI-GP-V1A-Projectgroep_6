e5ade830f279fbe4d42f2e67cdae3ce684cd77ec
Jamie3579 <74654404+Jamie3579@users.noreply.github.com>
2021-03-31 19:22:50 +0200

    File voor het aanmaken en vullen van de tabel popular

---
+++ user_story_4/popular/createRelationTables.py
@@ -0,0 +1,110 @@
+from rdbconnection2 import conrdb
+
+rdbcon, rdbcur = conrdb()
+
+query_popular_table = """
+DO $$
+DECLARE
+    _segment VARCHAR;
+    i INT;
+    index INT;
+    r RECORD;
+    clean_list VARCHAR[];
+    _combi VARCHAR[];
+    prod_freq_list VARCHAR[];
+    prod_freq VARCHAR[];
+    temp VARCHAR[];
+    temp2 INT[];
+    temp3 INT[];
+BEGIN
+    -- [ MAAK DE TABEL AAN OF UPDATE DE TABEL ]
+    DROP TABLE IF EXISTS popular CASCADE;
+    CREATE TABLE popular (
+        segment VARCHAR(255),
+        gender VARCHAR(255),
+        category VARCHAR(255),
+        product_id1 VARCHAR(255),
+        product_id2 VARCHAR(255),
+        product_id3 VARCHAR(255),
+        product_id4 VARCHAR(255),
+        FOREIGN KEY (product_id1)
+                REFERENCES product (product_id),
+        FOREIGN KEY (product_id2)
+                REFERENCES product (product_id),
+        FOREIGN KEY (product_id3)
+                REFERENCES product (product_id),
+        FOREIGN KEY (product_id4)
+                REFERENCES product (product_id)
+    );
+    -- [ VUL DE TABEL ]
+    ------------------------- || Voor elk segment bestaande binnen profielen
+    foreach _segment in ARRAY(select array(select lower(segment) as segment
+                                from profile
+                                group by lower(segment)
+                                order by lower(segment))) loop
+        ------------------------ || Groepeer elke mogelijk combinatie gender-categorie van producten
+        ------------------------ || gekocht door profielen met het segment _segment
+        foreach _combi slice 1 in ARRAY(select array(select array[lower(gender), lower(category)] from product
+                                        natural join(select product_id from orders
+                                        natural join(select * from sessions
+                                        natural join(select browser_id from buids
+                                        natural join profile where lower(segment)=_segment)
+                                        as _id) as __id) as ___id
+                                        group by lower(gender), lower(category)
+                                        order by lower(gender), lower(category))) loop
+            if _combi[1] is not Null and _combi[2] is not Null then -- || Als zowel het gender als de categorie niet Null zijn
+                ----------------------- || Neem dan de producten
+                prod_freq_list := array(select ARRAY[product_id, count(*)::varchar] from product
+                                        natural join(select product_id from orders
+                                        natural join(select * from sessions
+                                        natural join(select browser_id from buids
+                                        natural join profile where lower(segment)=_segment)
+                                        as _id) as __id) as ___id
+                                        where lower(gender)=_combi[1] and lower(category)=_combi[2]
+                                        group by product_id
+                                        order by product_id);
+            else -- || Als het gender Null is en de categorie niet
+                if _combi[1] is Null and _combi[2] is not Null then
+                    prod_freq_list := array(select ARRAY[product_id, count(*)::varchar] from product
+                                        natural join(select product_id from orders
+                                        natural join(select * from sessions
+                                        natural join(select browser_id from buids
+                                        natural join profile where lower(segment)=_segment)
+                                        as _id) as __id) as ___id
+                                        where lower(gender) is Null and lower(category)=_combi[2]
+                                        group by product_id
+                                        order by product_id);
+                else -- || Categorie Null bestaat niet op de front-end en is dus overbodig voor de tabel
+                    continue;
+                end if;
+            end if;
+            temp := ARRAY[Null];
+            temp2 := ARRAY[Null];
+            foreach prod_freq slice 1 in array(prod_freq_list) loop
+                    temp = array_append(temp, prod_freq[1]);
+                    temp2 = array_append(temp2, prod_freq[2]::int);
+            end loop;
+            temp3 = ARRAY(select distinct unnest(temp2) order by 1);
+            if array_length(temp3, 1) > 1 then
+                for i in 1..4 loop
+                    for r in select max(x) as f from unnest(temp2) as x loop
+                            index = array_position(temp2, r.f);
+                            clean_list[i] = temp[index];
+                            temp = array_remove(temp, temp[index]);
+                    end loop;
+                end loop;
+                if clean_list[1] is not Null then
+                    INSERT INTO popular VALUES (_segment, _combi[1], _combi[2], clean_list[1], clean_list[2], clean_list[3], clean_list[4]);
+                else
+                    continue;
+                end if;
+            end if;
+        end loop;
+    end loop;
+END $$;
+"""
+
+rdbcur.execute(query_popular_table)
+rdbcon.commit()
+rdbcur.close()
+rdbcon.close()
