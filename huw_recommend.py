69549b249e614b95adc31f98bc64d8b6050b07a5
Jamie3579 <74654404+Jamie3579@users.noreply.github.com>
2021-03-31 10:52:15 +0200

    Verbinding met tabel popular in rdb toegevoegd

--- huw_recommend.py
+++ huw_recommend.py
@@ -3,6 +3,9 @@ from flask_restful import Api, Resource, reqparse
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from rdbconnection2 import conrdb
+from collections import Counter
+import ast
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,18 +26,80 @@ else:
     client = MongoClient()
 database = client.huwebshop 
 
+def HighestFrequency(lijst):
+    """
+    Geeft het element met de hoogste frequentie in de lijst terug.
+    """
+    freq = Counter(lijst)
+    return freq.most_common(1)[0][0]
+
+def ReturnSelectExecution(sql_query, query_var):
+    """
+    Voert een query uit, met de meegegeven variabelen.
+    (Gaat uit van een 'SELECT' query waarbij data terug moet worden gehaald.)
+    Geeft de geselecteerde waardes terug.
+    """
+    rdbcon, rdbcur = conrdb()
+    rdbcur.execute(sql_query, query_var)
+    returnvalue = []
+    try:
+        returnvalue = rdbcur.fetchone()[0]
+        rdbcur.close()
+        rdbcon.close()
+    except TypeError:
+        pass
+    return returnvalue
+
 class Recom(Resource):
     """ This class represents the REST API that provides the recommendations for
     the webshop. At the moment, the API simply returns a random set of products
     to recommend."""
 
-    def get(self, profileid, count):
+    def get(self, profileid, count, type_rec, shopping_list, pagecat):
         """ This function represents the handler for GET requests coming in
-        through the API. It currently returns a random sample of products. """
-        randcursor = database.products.aggregate([{ '$sample': { 'size': count } }])
-        prodids = list(map(lambda x: x['_id'], list(randcursor)))
+        through the API. It currently returns a random sample of products. 
+        
+        :param type_rec: str : is één van de keys uit 'recommendationtypes' huw.py
+        :param shopping_list: str: str met daarin de boodschappenlijst """
+        
+        if type_rec == "popular":
+            pagecat = ast.literal_eval(pagecat)
+            cat = pagecat[0].replace("-", " ")
+            cat = cat.replace(" en ", " & ")
+            cat = cat.replace("make up", "make-up")
+            user_segment_query = """select lower(segment) from profile where profile_id=%s"""
+            user_segment = ReturnSelectExecution(user_segment_query, (profileid,))
+            user_gender_query = """
+            select array(select ARRAY[lower(gender), count(*)::varchar] from product
+                         natural join(select product_id from orders
+                         natural join(select session_id, segment from sessions
+                         natural join(select * from buids
+                         natural join profile
+                         where profile_id=%s) as _id) as __id) as ___id
+                         group by lower(gender)
+                         order by lower(gender));
+            """
+            user_gender_freq_list = ReturnSelectExecution(user_gender_query, (profileid,))
+            highest_freq = 0
+            user_gender = ''
+            for gender_freq in user_gender_freq_list:
+                freq = int(gender_freq[1])
+                if freq > highest_freq:
+                    highest_freq = freq
+                    user_gender = gender_freq[0]
+            
+            popular_query = """
+            select array[product_id1, product_id2, product_id3, product_id4] from popular
+            where lower(segment)=%s
+            and lower(gender)=%s
+            and lower(category)=%s;
+            """
+            prodids = ReturnSelectExecution(popular_query, (user_segment, user_gender, cat,))
+        else:
+            randcursor = database.products.aggregate([{ '$sample': { 'size': count } }])
+            prodids = list(map(lambda x: x['_id'], list(randcursor)))
         return prodids, 200
 
 # This method binds the Recom class to the REST API, to parse specifically
 # requests in the format described below.
-api.add_resource(Recom, "/<string:profileid>/<int:count>")
+api.add_resource(Recom, "/<string:profileid>/<int:count>/<string:type_rec>/<string:shopping_list>/<string:pagecat>")
